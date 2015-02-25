3c1e949a2fbd5cbccb7a2506351e5b79ac804239
Vlad Shcherbin <vlad.shcherbin@gmail.com>
2015-02-25 04:27:24 +0300

    update package.js

--- package.js
+++ package.js
@@ -1,22 +1,15 @@
 Package.describe({
   name: 'shcherbin:isotope',
-  version: '0.0.1',
-  // Brief, one-line summary of the package.
-  summary: '',
-  // URL to the Git repository containing the source code for this package.
-  git: '',
-  // By default, Meteor will default to using README.md for documentation.
-  // To avoid submitting documentation, set this field to null.
-  documentation: 'README.md'
+  version: '2.1.1',
+  summary: 'Isotope js wrapper for meteor. Different layouts: masonry, packery, fit columns / rows, etc. Filtering and sorting.',
+  git: 'https://github.com/VladShcherbin/isotopejs',
+  documentation: 'readme.md'
 });
 
 Package.onUse(function(api) {
-  api.versionsFrom('1.0.3.1');
-  api.addFiles('shcherbin:isotope.js');
-});
-
-Package.onTest(function(api) {
-  api.use('tinytest');
-  api.use('shcherbin:isotope');
-  api.addFiles('shcherbin:isotope-tests.js');
+  api.versionsFrom('1.0.1');
+  api.use('jquery', 'client');
+  api.addFiles([
+    'vendor/dist/isotope.pkgd.js'
+  ], 'client');
 });
